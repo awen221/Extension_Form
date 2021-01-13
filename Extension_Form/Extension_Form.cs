5389b2bf9911097a51495aaee8bc629fbb86b51d
larry.lai <larry.lai@advantech.com>
2021-01-13 16:14:41 +0800

    create

---
+++ Extension_Form/Extension_Form.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Extension_Form
+{
+	static public class Extension_Form
+	{
+		static public string GetFileVersionString(this Form form)
+		{
+			var assembly = Assembly.GetEntryAssembly();
+			var location = assembly.Location;
+			var fileVersionInfo = FileVersionInfo.GetVersionInfo(location);
+
+			return fileVersionInfo.FileVersion;
+		}
+	}
+}
+
